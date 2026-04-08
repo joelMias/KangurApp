931696edf3b35772951153d0360d661961364d9f
jomife <jomife@inspalamos.cat>
2026-04-08 11:47:01 +0200

    Build 23: Configurat Firebase a capacitor.config.ts amb dist

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.kangurapp.app',
   appName: 'KangurApp',
-  webDir: 'dist'
+  webDir: 'dist', 
+  plugins: {
+    FirebaseAuthentication: {
+      skipNativeAuth: false,
+      providers: ['password']
+    }
+  }
 };
 
 export default config;
